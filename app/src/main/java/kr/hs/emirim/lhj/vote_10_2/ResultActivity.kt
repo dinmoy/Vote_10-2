ab1399f6dadd8e1df5f512326ef79af813978027
dinmoy <imhyojin614@gmail.com>
2023-10-05 20:07:52 +0900

    투표하기 10-2 진행 중

---
+++ app/src/main/java/kr/hs/emirim/lhj/vote_10_2/ResultActivity.kt
@@ -0,0 +1,12 @@
+
+package kr.hs.emirim.lhj.vote_10_2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
